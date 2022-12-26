355875c94fb87c9c67d3c0516b8ce966e4fd91ac
Bo Latt <bolattny@gmail.com>
2022-12-26 16:40:46 -0500

    add create recipe page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,39 @@
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState("");
+  const [formError, setFormError] = useState(null);
+
   return (
     <div className="page create">
       <h2>Create</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title"> Title: </label>
+        <input
+          id="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="method"> Method: </label>
+        <input
+          id="method"
+          type="text"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+        <label htmlFor="title"> Rating: </label>
+        <input
+          id="rating"
+          type="number"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+        <button>Create Smoothie Recipe</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
